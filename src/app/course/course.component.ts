0ec7b4f6860e1a5762505b32755c6f2db8336fc1
Hanin Farhan <haninfarhan96@gmail.com>
2024-05-05 15:54:47 +0200

    lagt till click funktion för sortering

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,24 +1,41 @@
 import { Component } from '@angular/core';
 import { CourseInfo } from '../models/course-info';
 import { CourseService } from '../services/course.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-course',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './course.component.html',
   styleUrl: './course.component.css'
 })
 export class CourseComponent {
   courses: CourseInfo[] = [];
+  sortedCourses: CourseInfo[] = [];
 
   constructor(private courseService: CourseService) { }
 
   ngOnInit() {
     this.courseService.getCourses().subscribe((courses) => {
       this.courses = courses;
-      console.log(this.courses);
+      this.sortedCourses = courses;
     });
+  }
+
+  sort(type: string): void {
+    this.sortedCourses = this.courses.sort((courseA, courseB) => {
+      if(type === 'code') {
+        return courseA.code.localeCompare(courseB.code);
+
+      } else if(type=== 'coursename') {
+        return courseA.coursename.localeCompare(courseB.coursename);
+
+      } else {
+        return courseA.progression.localeCompare(courseB.progression);
+      }
+      
+    })
   }
 
 }
